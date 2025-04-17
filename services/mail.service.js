63e51345ed0f03b28669abfee95ff91b33c72ce9
bnethanel <bnethanel@gmail.com>
2025-04-17 10:48:00 +0300

    add mail service with basic data

---
+++ services/mail.service.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const mail = {
+    id: 'e101',
+    createdAt: 1551133930500,
+    subject: 'Miss you!',
+    body: 'Would love to catch up sometimes',
+    isRead: false,
+    sentAt: 1551133930594,
+    removedAt: null,
+    from: 'momo@momo.com',
+    to: 'user@appsus.com'
+}
+
+const loggedinUser = {
+    email: 'user@appsus.com',
+    fullname: 'Mahatma Appsus'
+}
+
