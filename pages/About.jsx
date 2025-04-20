b05a183f789e63a64ce4cc21b7c4a333ebfd8233
bnethanel <bnethanel@gmail.com>
2025-04-20 20:48:44 +0300

    add content & basic style to about & home pages

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,6 +1,10 @@
 
 export function About() {
     return <section className="about">
-        <h1>About Page</h1>
+            <p className="about-section">Welcome to Appsus, your all-in-one solution for productivity and organization.
+                Whether you're managing your emails, taking notes, or staying on top of daily tasks, Appsus is here to simplify your digital life.
+
+                With a sleek interface and mobile-friendly design, Appsus combines the power of a mail client and note-taking app in one seamless experience.
+                Our goal is to help you stay focused, save time, and keep everything important in one place.</p>
     </section>
 }
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -2,5 +2,10 @@
 export function Home() {
     return <section className="home">
         <h1>Welcome to home page!</h1>
+        <p>Hope you enjoy!!</p>
+        <div className="img-container flex align-center justify-center">
+            <img className="home-logo" src="../assets/img/Appsus-logo.png" alt="" />
+        </div>
+
     </section>
 }
